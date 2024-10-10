337b1549abb3f553c2554598eadb2b8535f6b3a2
ThePeakBali <thepeakbali@gmail.com>
2024-10-10 18:49:05 +0800

    fix sidebar's animation and nav's size

--- javascript/ThePeakBali.js
+++ javascript/ThePeakBali.js
@@ -1,43 +1,37 @@
 (() => {
     /* ---- Navigation Behaviour ---- */
+    // TODO
+    // Refactor all of this so it looks cleaner.
     const navigationSidebar = () => {
         const buttonToggler = document.querySelector("button.navigation-button-toggle");
         const list = document.querySelector(".navigation-list").cloneNode(true);
         list.classList.add("navigation-collapse");
         let isShowed = false;
-        // TODO:
-        //  Use promise, await, or asnyc to wait for the 'list' node
-        //  to be added, and then we apply for the pop-up animation.
-        //  That goes the same for removing the node.
-        // Note:
-        // I have zero idea why this works.
+        let onGoingEvent = false;
         buttonToggler.addEventListener("click", function() {
+            if(onGoingEvent) return;
             if(isShowed) {
                 const closeSidebar = new Promise((resolve) => {
                     list.classList.remove("collapse-show");
-                    setTimeout(() => { 
-                        if(isShowed) {
-                            resolve(); 
-                        }
-                    }, 0);
+                    setTimeout(resolve, 250);
+                    onGoingEvent = true;
                 });
                 closeSidebar.then(() => {
-                    isShowed = false;
                     buttonToggler.removeChild(list);
+                    isShowed = false;
+                    onGoingEvent = false;
                 });
             }
             else {
                 const openSidebar = new Promise((resolve) => {
                     buttonToggler.append(list);
-                    setTimeout(() => {
-                        if(!isShowed) {
-                            resolve();
-                        }
-                    }, 0);
+                    setTimeout(resolve, 0);
+                    onGoingEvent = true;
                 });
                 openSidebar.then(() => {
-                    isShowed = true;
                     list.classList.add("collapse-show");
+                    isShowed = true;
+                    onGoingEvent = false;
                 });
             }
         });
