7a2d31c036da2acbad8d780714966fa283613974
ThePeakBali <thepeakbali@gmail.com>
2024-10-10 18:11:27 +0800

    fixing a sidebar bug.

--- javascript/ThePeakBali.js
+++ javascript/ThePeakBali.js
@@ -9,27 +9,38 @@
         //  Use promise, await, or asnyc to wait for the 'list' node
         //  to be added, and then we apply for the pop-up animation.
         //  That goes the same for removing the node.
+        // Note:
+        // I have zero idea why this works.
         buttonToggler.addEventListener("click", function() {
             if(isShowed) {
-                list.classList.remove("collapse-show");
-                setTimeout(() => {
+                const closeSidebar = new Promise((resolve) => {
+                    list.classList.remove("collapse-show");
+                    setTimeout(() => { 
+                        if(isShowed) {
+                            resolve(); 
+                        }
+                    }, 0);
+                });
+                closeSidebar.then(() => {
+                    isShowed = false;
                     buttonToggler.removeChild(list);
-                }, 300)
-                isShowed = false;
+                });
             }
             else {
-                buttonToggler.append(list);
-                setTimeout(() => {
+                const openSidebar = new Promise((resolve) => {
+                    buttonToggler.append(list);
+                    setTimeout(() => {
+                        if(!isShowed) {
+                            resolve();
+                        }
+                    }, 0);
+                });
+                openSidebar.then(() => {
+                    isShowed = true;
                     list.classList.add("collapse-show");
-                }, 10);
-                isShowed = true;
+                });
             }
         });
     };
     navigationSidebar();
-
-    const navigationList = () => {
-        
-    };
-    navigationList();
 })();
